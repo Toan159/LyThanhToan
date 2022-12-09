8ba1ee1c6ac241e37ed37f69239438f3158f3b51
DESKTOP-SJTNIIR\toan <thtoan159@gmail.com>
2022-12-09 09:48:58 +0700

    complete cắt mới 1 module có dùng slick slider

---
+++ sagethem/app/public/wp-content/uploads/cache/57803736b004d299d782c3df7fe3a38d671cf928.php
@@ -0,0 +1,119 @@
+<div class="container">
+    <section class="m-32">
+        <div class="header"></div>
+        <div class="nav">
+            <div class="nav-1">
+                <div class="btn-nav"></div>
+                <h1>
+                    <!-- pr-40 là -> padding-right: 80px; h-96 -> height: 192px; -->
+                    The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:
+                </h1>
+            </div>
+
+            <div class="nav-1-1">
+                <div class="nav-2">
+                    <div class="nav-2-1">
+                        <h2>
+                            <!-- h-16 -> height: 32px; -->
+                            Drive Business Growth
+                        </h2>
+                        <p class="article">
+                            Tap into the power of predictive analytics and
+                            demand forecating. Cappitalize on revenue-
+                            generaitng opportunities faster. Capture mission-
+                            critical data across your entire operation-including all three
+                            suppy chain tiers- empowering you to bring your beverage or food business into
+                            new levels of profitability.
+                        </p>
+                    </div>
+                    <div class="nav-2-1">
+                        <h3>
+                            Leverage Real-Time Data for Better Decision-Making
+                        </h3>
+                        <p class="article">
+                            Accelerate business decisions with better, more accurate
+                            system-to-system data and comprehensive reporting that
+                            results in deep, actionable insights. Stay ahead of
+                            potential disruptions and maintain a finger oon the pulse
+                            of your supply chain from anywhere in the world.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="nav-2">
+                    <div class="nav-2-1">
+                        <h4>Improve Operational Efficioncies</h4>
+                        <p class="article">Streamline prociesses and save money by reducing
+                            leakage with a fully integrated ERP system that works for your
+                            business. Encompass ERP makes it prossible to identily
+                            where, when, and how resources can be best
+                            allocated to provide the most optimal outcome.
+                        </p>
+                    </div>
+
+                    <div class="nav-2-1">
+                        <h5>Enhance Team Dynamics & Employee Satisfaction</h5>
+                        <p class="article"> When your labor force has the tools they need to succeed,
+                            the workplace runs like a well- olied machine. With
+                            Encompass ERP, every stackeholder across your
+                            supply chain-from purchasing to Point-of-Sale-
+                            has what they need to do their jobs effectively.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="slick-slider-demo ">
+            <div class="conten-slick-demo">
+                <p class="text">"We've done more with Encompas in the past 6 months than we
+                    did in 3 years on our previous solution."</p>
+                <p class="text-1">David Little <br> CTO, Savannah Distributing </p>
+            </div>
+            <div class="conten-slick-demo">
+                <p class="text">"We've done more with Encompas in the past 7 months than we
+                    did in 3 years on our previous solution."</p>
+                <p class="text-1">David Little <br> CTO, Savannah Distributing </p>
+            </div>
+            <div class="conten-slick-demo">
+                <p class="text">"We've done more with Encompas in the past 8 months than we
+                    did in 3 years on our previous solution."</p>
+                <p class="text-1">David Little <br> CTO, Savannah Distributing </p>
+            </div>
+            <div class="conten-slick-demo">
+                <p class="text">"We've done more with Encompas in the past 9 months than we
+                    did in 3 years on our previous solution."</p>
+                <p class="text-1">David Little <br> CTO, Savannah Distributing </p>
+            </div>
+        </div>
+
+
+        <div class="Our-Open">
+            <div>
+                <span class="content-our-open">
+                    Our Open Ecosystem of Partners Gives You the Power to Choose
+                </span>
+            </div>
+            <div class="content-our-open-1">
+                <div class="content-our-open-p">
+                    <p>We believe in the power of transparency and connectedness. Encompass’ ecosystem of partners
+                        combines diversity and best-in-class API functionality to provide one powerful,
+                        unified source of truth for our clients, no matter how many modules they choose to utilize.</p>
+                    <p>
+                        With Encompass ERP, you choose only the tools and partners you need, leaving out what isn’t necessary.
+                        Finally, your beverage or food supply chain can now be managed with a single sign-on experience without
+                        compromising efficiency or breaking data flow.</p>
+                </div>
+                <div class="content-our-open-image">
+                    <img >
+                    <img >
+                    <img >
+                    <img >
+                    <img >
+                    <img >
+                </div>
+            </div>
+        </div>
+        <div class="footer"></div>
+    </section>
+</div>
